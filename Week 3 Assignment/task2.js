13c1c60e3e6c288242a80df9b9cb639b09e4f80f
sagarrrr21 <chauhansagarsingh21@gmail.com>
2026-03-09 22:17:14 +0530

    task2 commit

--- Week 3 Assignment/task2.js
+++ Week 3 Assignment/task2.js
@@ -1,17 +1,17 @@
 function getUniqueElements(arr1, arr2) {
-    let combined = [...arr1, ...arr2];
-    let unique = [];
+  let combined = [...arr1, ...arr2];
+  let unique = [];
 
-    for (let num of combined) {
-        if (combined.indexOf(num) === combined.lastIndexOf(num)) {
-            unique.push(num);
-        }
+  for (let num of combined) {
+    if (combined.indexOf(num) === combined.lastIndexOf(num)) {
+      unique.push(num);
     }
+  }
 
-    return unique;
+  return unique;
 }
 
 let a = [1, 2, 3, 4];
 let b = [3, 4, 5, 6];
 
-console.log(getUniqueElements(a, b));
+console.log("Unique Elements : " + getUniqueElements(a, b));
